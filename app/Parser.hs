9154d5d5871378c978e89e3bdeea2cd29e9e7231
czheo <1692400+czheo@users.noreply.github.com>
2022-12-09 09:35:00 -0800

    add arith, number, boolean expr

--- app/Parser.hs
+++ app/Parser.hs
@@ -1,9 +1,13 @@
 module Parser where
+import Control.Applicative
+import Data.Char
+
 data Expr = Number Double
-          | Add Expr
-          | Sub Expr
-          | Mul Expr
-          | Div Expr
+          | Add Expr Expr
+          | Sub Expr Expr
+          | Mul Expr Expr
+          | Div Expr Expr
+          | Boolean Bool
           deriving Show
 
 data Src =
@@ -30,20 +34,63 @@ instance Applicative Parser where
                    (a, src'') <- p2 src'
                    return (f a, src'')
 
+instance Alternative Parser where
+  empty = Parser (const Nothing)
+  Parser p1 <|> Parser p2 = Parser f
+    where f src = p1 src <|> p2 src
+
+instance Monad Parser where
+  Parser p >>= f = Parser p'
+    where p' src = case p src of
+                   Nothing -> Nothing
+                   Just (a, src) -> runP (f a) src
+
 src = Src {line = 0, col = 0, text = undefined}
 
-charP :: Char -> Parser Char
-charP ch = Parser f
-  where f src@Src{text = c:rem} | c == ch = Just (c, src')
+ifCharP :: (Char -> Bool) -> Parser Char
+ifCharP predicate = Parser f
+  where f src@Src{text = c:rem} = if predicate c then Just (c, src') else Nothing
           where src' = case c of
                        '\n' -> src {line = line src + 1, col = 0, text = rem}
                        _ -> src {col = col src + 1, text = rem}
         f _ = Nothing
 
+charP :: Char -> Parser Char
+charP c = ifCharP (==c)
+
 stringP :: String -> Parser String
 stringP = sequenceA . map charP
 
+keywordP :: String -> Expr -> Parser Expr
+keywordP s exp = const exp <$> stringP s
+
+spanP :: (Char -> Bool) -> Parser String
+spanP pred = some $ ifCharP pred
+
+boolExpr :: Parser Expr
+boolExpr = trueExpr <|> falseExpr
+    where trueExpr = keywordP "true" $ Boolean True
+          falseExpr = keywordP "false" $ Boolean False
+
+numberExpr :: Parser Expr
+numberExpr = (\s -> Number $ read s) <$> spanP isDigit
+
+arithExpr :: Parser Expr
+arithExpr = do
+  n1 <- numberExpr
+  many $ charP ' '
+  op <- ifCharP (\c -> c `elem` "+-*/")
+  many $ charP ' '
+  n2 <- numberExpr
+  return $ (toOp op) n1 n2
+  where toOp '+' = Add
+        toOp '-' = Sub
+        toOp '*' = Mul
+        toOp '/' = Div
+
+exprP = arithExpr <|> numberExpr <|> boolExpr
+
 parse :: String -> Maybe Expr
 parse text = do
-  (num, _) <- runP (read <$> stringP "123") src {text = text}
-  return $ Number num
+  (expr, _) <- runP exprP src {text = text}
+  return expr
